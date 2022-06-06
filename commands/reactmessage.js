6d0651d83d0f8dacfde9cfc3f9b62e0efb260f30
SpadeInTheDirt <77943991+SpadeInTheDirt@users.noreply.github.com>
2022-06-06 09:40:50 -0400

    added features and bugfixes

--- commands/reactmessage.js
+++ commands/reactmessage.js
@@ -1,8 +1,12 @@
+const fs = require("fs");
+const wait = require('node:timers/promises').setTimeout;
+
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { getVoiceConnection, joinVoiceChannel } = require('@discordjs/voice');
-const fs = require("fs");
+const { PermissionFlagsBits } = require('discord-api-types/v10');
 
-const contents = fs.readFileSync('charEmojis.json', 'utf8');
+
+const contents = fs.readFileSync('charEmojis.json');
 const charEmojis = JSON.parse(contents);
 
 module.exports = {
@@ -19,18 +23,20 @@ module.exports = {
           option.setName('message')
             .setDescription('message to react in emoji')
             .setRequired(true)),
-  async execute(interaction) {echo(interaction)},
+  execute: reactmessage,
 };
 
-async function echo(interaction) {
-  const messageId = interaction.options.getString('messageid');
-  const reactMessage = interaction.options.getString('message').toUpperCase();
+async function reactmessage(interaction, client) {
+  interaction.deferReply();
 
+  const messageId = await interaction.options.getString('messageid');
+  const reactMessage = await interaction.options.getString('message').toUpperCase();
+  
   console.log("reactMessage: finding messageid " + messageId);
   
-  var success = false;
+  let success = false;
 
-  msg = await interaction.channel.messages.fetch(messageId)
+  let msg = await interaction.channel.messages.fetch(messageId)
   .then(
     (v) => {
       success = true;
@@ -43,40 +49,103 @@ async function echo(interaction) {
   );
 
   if (!success) {
-    await interaction.reply({ content: "i couldn't find that message...", ephemeral: true });
+    console.log("failed to find message");
+    await interaction.editReply({ content: "I couldn't find that message...", ephemeral: true });
     return;
   }
 
-  for (i of reactMessage) {
-    
-    if ((/[A-Z ]/).test(i)) {
-    
-    // (charEmojis.keys().includes(i)) {
-      console.log("reacting: " + i + " as " + charEmojis[i]);
-      
-      msg.react(charEmojis[i])
-      .then(
-        (v) => {
-          success = true;
-          return v;
-        },
-        (error) =>
-        {
-          success = false;
-          console.error("echo message react failed");
+  console.log("message found: " + msg.content);
+
+  let tempCharEmojis = charEmojis;
+  let unrecognizedChars = 0;
+  let failedChars = 0;
+  let reactedEmojis = [];
+
+  // message check iterate
+  for (i = 0; i < reactMessage.length;) {
+    let reactEmoji = null;
+
+    // emulation of for else loop:
+    find: {
+      // check keys: a search for reactEmoji's val
+      for (checkKey in charEmojis) {
+        // if key matches respective slice
+        if (checkKey == reactMessage.slice(i, i + checkKey.length)) {
+          reactEmoji = tempCharEmojis[checkKey][0];
+
+          let doReact = false;
+
+          // since key matches, reroll until a matching emoji that hasnt been reacted yet appears or no more emojis
+          while (reactEmoji && !doReact) {
+            // remove the emoji from the pools of available characters
+            for (removeKey in tempCharEmojis) {
+              tempCharEmojis[removeKey] = tempCharEmojis[removeKey].filter((elem) => (elem != reactEmoji));
+            }
+
+            // check if react doesnt exist among existing reactables
+            doReact = true;
+            for ([emoji, reaction] of msg.reactions.cache) {
+              if (emoji == reactEmoji) {
+                console.log(reactEmoji + " reaction already reacted");
+                doReact = false;
+                break;
+              }
+            }
+
+            if (!doReact) reactEmoji = tempCharEmojis[checkKey][0];
+          }
+
+          // if react was found that works, react
+          if (doReact) {
+            await msg.react(reactEmoji)
+            .then(
+              (v) => {
+                success = true;
+                return v;
+              },
+              (error) =>
+              {
+                success = false;
+                console.error(error);
+              }
+            );
+
+            if (!success) {
+              await interaction.editReply({ content: "I failed a react to that message...", ephemeral: true });
+              return;
+            }
+
+            reactedEmojis.push(reactEmoji);
+
+            console.log(checkKey + " reacted as " + reactEmoji);
+            i += checkKey.length;
+            break find;
+          } else {
+            console.log(checkKey + " failed");
+            ++i;
+            ++failedChars;
+            break find;
+          }
         }
-      );
-      
-      if (!success) {
-        await interaction.reply({ content: "i couldn't react to that message...", ephemeral: true });
-        return;
       }
+      // emulation of for else loop: else block
+      console.log(reactMessage[i] + " unrecognized");
+      ++i;
+      ++unrecognizedChars;
     }
   }
 
+  let outMsg = "Completed reacting " + reactMessage + " : removing in 20 seconds\n";
 
-  await interaction.reply({ content: 'completed: removing in 20 seconds', ephemeral: true });
+  if (unrecognizedChars > 0) outMsg += unrecognizedChars + " were unrecognized\n";
+  if (failedChars > 0) outMsg += failedChars + " failed to be reacted\n";
 
+  await interaction.editReply({ content:outMsg, ephemeral: true });
+  await wait(20000);
 
-  
+  console.log("removing")
+  for (emoji of reactedEmojis) {
+    await msg.reactions.cache.get(emoji).users.remove(client.user.id);
+  }
+
 }
